c8ec5c38095ccdb9e7f8fa47b4534f4314d218b1
DevAndrey9E <128442731+DevAndrey9E@users.noreply.github.com>
2023-03-29 03:38:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ligarDesligar() {
+  alert("Ligando/Desligando...");
+}
+
+function abrirCamera() {
+  alert("Abrindo câmera...");
+}
+
+function abrirNavegador() {
+  window.open("https://www.google.com/", "_blank");
+}
